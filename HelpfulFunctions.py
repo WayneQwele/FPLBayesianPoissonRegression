702ce599353623b921e0118d0ad8c4c07d38fdad
Wayne <wayneqwele@protonmail.com>
2023-10-03 15:07:51 +0200

    Early preview edition plan to circulate to a few friends to have a look.

--- HelpfulFunctions.py
+++ HelpfulFunctions.py
@@ -4,7 +4,7 @@
 
 import pandas as pd
 
-def clean_and_convert_data(filename):
+def clean_and_convert_data(filename: str):
     """ 
     This function preps and cleans the save CSV files. 
     """
@@ -20,6 +20,7 @@ def clean_and_convert_data(filename):
     # Convert 'kickoff_time' and 'kickoff_date' columns to datetime dtype
     game_df['kickoff_time'] = pd.to_datetime(game_df['kickoff_time'])
     game_df['kickoff_date'] = pd.to_datetime(game_df['kickoff_date'])
+    
 
     # Rename 'row_count' to 'matchgames'
     game_df = game_df.rename(columns={'row_count': 'matchgames'})
@@ -27,5 +28,15 @@ def clean_and_convert_data(filename):
     return game_df
 
 
+def total_point_correction(row: int):
+    '''
+    To correct issue with -ve points which is not condusive to my model, It is
+    to be used when capping total_points from our final model.
+    '''
+    if row <0:
+        result = 0
+    else:
+        result = row
+    return result
 
 
